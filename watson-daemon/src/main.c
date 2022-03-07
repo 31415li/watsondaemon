99c7acf90e1494a3a5414d6d188de63f27ced9c5
RuntimeTantrum <runtimetantrum@gmail.com>
2022-03-07 09:46:26 +0200

    Basic reporting to Watson

--- watson-daemon/src/main.c
+++ watson-daemon/src/main.c
@@ -1,6 +1,29 @@
-#include <stdio.h>
+#include <iotp_device.h>
 
 int main(void) {
-    printf("Heyo!\n");
+    IoTPConfig *config = NULL;
+    IoTPDevice *device = NULL;
+    
+    IoTPConfig_create(&config, "/etc/config/watson-daemon.yaml");
+    IoTPDevice_create(&device, config);
+    IoTPDevice_connect(device);
+    
+    // MQTTProperties *properties = (MQTTProperties *)malloc(sizeof(MQTTProperties));
+    // MQTTProperty   property;
+    // property.identifier = MQTTPROPERTY_CODE_USER_PROPERTY;
+    // property.value.data.data = "user defined property";
+    // property.value.data.len = (int)strlen(property.value.data.data);
+    // property.value.value.data = "user defined property value";
+    // property.value.value.len = (int)strlen(property.value.value.data);
+    // MQTTProperties_add(properties, &property);
+
+    char *data = "{\"d\" : {\"SensorID\": \"Test\", \"Reading\": 7 }}";
+    IOTPRC err = IoTPDevice_sendEvent(device, "status", data, "json", QoS0, NULL);
+    printf("%d\n", err);
+
+    IoTPDevice_disconnect(device);
+    IoTPDevice_destroy(device);
+    IoTPConfig_clear(config);
+
     return 0;
 }
