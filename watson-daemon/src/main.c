1426427007ab22ac2d146de839450bf37799ecbe
RuntimeTantrum <runtimetantrum@gmail.com>
2022-03-08 11:56:51 +0200

    watson daemon ubus fetching

--- watson-daemon/src/main.c
+++ watson-daemon/src/main.c
@@ -2,6 +2,8 @@
 #include <iotp_device.h>
 #include <uci.h>
 #include <syslog.h>
+#include <libubox/blobmsg_json.h>
+#include <libubus.h>
 
 int tranfer_config_fields(struct uci_context *context, struct uci_section *section, IoTPConfig *config) {
     const int field_count = 4;
@@ -131,6 +133,86 @@ err1:
     return NULL;
 }
 
+
+typedef struct system_data {
+    bool valid;
+    int64_t total_memory;
+    int64_t free_memory;
+} system_data;
+
+enum {
+    INFO_MEMORY,
+    __INFO_MAX
+};
+
+enum {
+    MEMORY_TOTAL,
+    MEMORY_FREE,
+    __MEMORY_MAX
+};
+
+static struct blobmsg_policy info_policy[__INFO_MAX] = {
+    [INFO_MEMORY] = { .name = "memory", .type = BLOBMSG_TYPE_TABLE }
+};
+
+static struct blobmsg_policy memory_policy[__MEMORY_MAX] = {
+    [MEMORY_TOTAL] = { .name = "total", .type = BLOBMSG_TYPE_INT64 },
+    [MEMORY_FREE]  = { .name = "free",  .type = BLOBMSG_TYPE_INT64 }
+};
+
+void parse_system_data(struct ubus_request *req, int type, struct blob_attr *msg) {
+    system_data *data = (system_data*) req->priv;
+    struct blob_attr *info_table[__INFO_MAX];
+    struct blob_attr *memory_table[__MEMORY_MAX];
+
+    blobmsg_parse(info_policy, __INFO_MAX, info_table, blob_data(msg), blob_len(msg));
+
+    if (!info_table[INFO_MEMORY]) {
+        goto err;
+    }
+
+    blobmsg_parse(memory_policy, __MEMORY_MAX, memory_table, 
+                  blobmsg_data(info_table[INFO_MEMORY]),
+                  blobmsg_len(info_table[INFO_MEMORY]));
+
+    if (!memory_table[MEMORY_TOTAL] || !memory_table[MEMORY_FREE]) {
+        goto err;
+    }
+
+    data->valid = true;
+    data->total_memory = blobmsg_get_u64(memory_table[MEMORY_TOTAL]);
+    data->free_memory  = blobmsg_get_u64(memory_table[MEMORY_FREE]);
+    return;
+
+err:
+    syslog(LOG_ERR, "Could not fetch system data from ubus");
+
+    data->valid = false;
+    data->total_memory = 0;
+    data->free_memory = 0;
+}
+
+int fetch_system_data(struct ubus_context *ctx, system_data *out_data) {
+    const char * const service_name = "system";
+    const char * const method_name = "info";
+    uint32_t id;
+    int err;
+
+    err = ubus_lookup_id(ctx, service_name, &id);
+    if (err) {
+        syslog(LOG_ERR, "Could not find '%s' ubus service", service_name);
+        return 1;
+    }
+
+    err = ubus_invoke(ctx, id, method_name, NULL, (void*) parse_system_data, out_data, 3000);
+    if (err) {
+        syslog(LOG_ERR, "Could not invoke ubus method");
+        return 1;
+    }
+
+    return 0;
+}
+
 int main(void) {
     IoTPConfig *config = NULL;
     IoTPDevice *device = NULL;
@@ -150,10 +232,23 @@ int main(void) {
         rc = EXIT_FAILURE;
         goto err2;
     }
+
+    struct ubus_context *ctx = ubus_connect(NULL);
+
+    while (true) {
+        system_data data;
+        fetch_system_data(ctx, &data);
+
+        char buf[256];
+        sprintf(buf, "{\"total\": %lld, \"free\": %lld }", data.total_memory, data.free_memory);
     
-    char *data = "{\"d\" : {\"SensorID\": \"Test\", \"Reading\": 7 }}";
-    IOTPRC err = IoTPDevice_sendEvent(device, "status", data, "json", QoS1, NULL);
-    
+        IOTPRC err = IoTPDevice_sendEvent(device, "status", buf, "json", QoS0, NULL);
+
+        sleep(2);
+    }
+
+    ubus_free(ctx);
+
     disconnect_device(device);
 err2:
     clear_config(config);
