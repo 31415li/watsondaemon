dce8751576af7d31e42e1f609da9ec738266b524
RuntimeTantrum <runtimetantrum@gmail.com>
2022-03-08 10:25:01 +0200

    watson-daemon logging

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 RUTOS_ROOT_DIR:=~/proj/rutos
 IOTP_ROOT_DIR:= $(RUTOS_ROOT_DIR)/build_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/libibm-iotp-1.0.0/ipkg-arm_cortex-a7_neon-vfpv4/libibm-iotp
 DAEMON_ROOT_DIR:=$(RUTOS_ROOT_DIR)/build_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/watson-daemon-1.0.0/ipkg-arm_cortex-a7_neon-vfpv4/watson-daemon
+CPFLAGS:=--preserve=timestamps
 
 load-iotp:
-	cp -rv libibm-iotp $(RUTOS_ROOT_DIR)/package
+	cp $(CPFLAGS) -rv libibm-iotp $(RUTOS_ROOT_DIR)/package
 	
 load-daemon:
-	cp -rv watson-daemon $(RUTOS_ROOT_DIR)/package
+	cp $(CPFLAGS) -rv watson-daemon $(RUTOS_ROOT_DIR)/package
 
 build-iotp: load-iotp
 	make -C $(RUTOS_ROOT_DIR) package/libibm-iotp/compile
--- watson-daemon/src/main.c
+++ watson-daemon/src/main.c
@@ -1,58 +1,164 @@
 #include <stdio.h>
 #include <iotp_device.h>
 #include <uci.h>
+#include <syslog.h>
+
+int tranfer_config_fields(struct uci_context *context, struct uci_section *section, IoTPConfig *config) {
+    const int field_count = 4;
+    const char *uci_fields[] = {
+        "orgId", "typeId", "deviceId", "token"
+    };
+    const char *iotp_fields[] = {
+        "identity.orgId", "identity.typeId", "identity.deviceId", "auth.token"
+    };
+
+    for (int i = 0; i < field_count; i++) {
+        IOTPRC rc;
+        const char *value = uci_lookup_option_string(context, section, 
+                                                     uci_fields[i]);
+        if (!value) {
+            syslog(LOG_CRIT, "Could not find field '%s' in config",
+                   uci_fields[i]);
+            return 1;
+        }
+
+        rc = IoTPConfig_setProperty(config, iotp_fields[i], value);
+        if (rc != IOTPRC_SUCCESS) {
+            syslog(LOG_CRIT, "Failed to set IoTP property '%s': %s",
+                   iotp_fields[i], IOTPRC_toString(rc));
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void clear_config(IoTPConfig *config) {
+    IOTPRC rc;
+    rc = IoTPConfig_clear(config);
+    if (rc != IOTPRC_SUCCESS) {
+        syslog(LOG_ERR, "Could not clean up IoTP config object: %s",
+               IOTPRC_toString(rc));
+    }
+}
 
 IoTPConfig *load_iotp_config(void) {
     const char *const config_name = "watson-daemon";
-    struct uci_context *context = uci_alloc_context();
+    const char *const section_name = "global";
+    struct uci_context *context;
     struct uci_package *package;
+    struct uci_section *section;
+    IoTPConfig *config = NULL;
+    IOTPRC rc;
+
+    context = uci_alloc_context();
+    if (!context) {
+        syslog(LOG_CRIT, "Failed to allocate UCI context");
+        return NULL;
+    }
 
     uci_load(context, config_name, &package);
+    if (!package) {
+        syslog(LOG_CRIT, "Failed to load config file");
+        goto err1;
+    }
 
-    struct uci_section *section = uci_lookup_section(context, package, "global");
+    section = uci_lookup_section(context, package, section_name);
+    if (!section) {
+        syslog(LOG_CRIT, "Could not find section '%s'", section_name);
+        goto err1;
+    }
 
-    char *org_id = uci_lookup_option_string(context, section, "orgId");
-    char *type_id = uci_lookup_option_string(context, section, "typeId");
-    char *device_id = uci_lookup_option_string(context, section, "deviceId");
-    char *token = uci_lookup_option_string(context, section, "token");
-    
-    IoTPConfig *config = NULL;
-    IoTPConfig_create(&config, NULL);
+    rc = IoTPConfig_create(&config, NULL);
+    if (rc != IOTPRC_SUCCESS) {
+        syslog(LOG_CRIT, "Could not create IoTP config object: %s",
+               IOTPRC_toString(rc));
+        goto err1;
+    }
 
-    IoTPConfig_setProperty(config, "identity.orgId", org_id);
-    IoTPConfig_setProperty(config, "identity.typeId", type_id);
-    IoTPConfig_setProperty(config, "identity.deviceId", device_id);
-    IoTPConfig_setProperty(config, "auth.token", token);
+    int err = tranfer_config_fields(context, section, config);
+    if (err) {
+        goto err2;
+    }
 
     uci_free_context(context);
-
     return config;
+
+err2:
+    clear_config(config);
+err1:
+    uci_free_context(context);
+    return NULL;
+}
+
+void disconnect_device(IoTPDevice *device) {
+    IOTPRC rc;
+    
+    rc = IoTPDevice_disconnect(device);
+    if (rc != IOTPRC_SUCCESS) {
+        syslog(LOG_ERR, "Could not disconnect IoTP device: %s",
+               IOTPRC_toString(rc));
+    }
+
+    rc = IoTPDevice_destroy(device);
+    if (rc != IOTPRC_SUCCESS) {
+        syslog(LOG_ERR, "Could not clean up IoTP device object: %s",
+               IOTPRC_toString(rc));
+    }
+}
+
+IoTPDevice *connect_device(IoTPConfig *config) {
+    IoTPDevice *device = NULL;
+    IOTPRC rc;
+    
+    rc = IoTPDevice_create(&device, config);
+    if (rc != IOTPRC_SUCCESS) {
+        syslog(LOG_CRIT, "Could not create IoTP device: %s",
+               IOTPRC_toString(rc));
+        return NULL;
+    }
+    
+    rc = IoTPDevice_connect(device);
+    if (rc != IOTPRC_SUCCESS) {
+        syslog(LOG_CRIT, "Could not connect to IoTP server: %s",
+               IOTPRC_toString(rc));
+        goto err1;
+    }
+
+    return device;
+
+err1:
+    IoTPDevice_destroy(device);
+    return NULL;
 }
 
 int main(void) {
-    IoTPConfig *config = load_iotp_config();
+    IoTPConfig *config = NULL;
     IoTPDevice *device = NULL;
-    
-    IoTPConfig_create(&config, "/etc/config/watson-daemon.yaml");
-    IoTPDevice_create(&device, config);
-    IoTPDevice_connect(device);
-    
-    // MQTTProperties *properties = (MQTTProperties *)malloc(sizeof(MQTTProperties));
-    // MQTTProperty   property;
-    // property.identifier = MQTTPROPERTY_CODE_USER_PROPERTY;
-    // property.value.data.data = "user defined property";
-    // property.value.data.len = (int)strlen(property.value.data.data);
-    // property.value.value.data = "user defined property value";
-    // property.value.value.len = (int)strlen(property.value.value.data);
-    // MQTTProperties_add(properties, &property);
+    int rc = EXIT_SUCCESS;
 
+    openlog("watson-daemon", LOG_PID, LOG_DAEMON);
+    syslog(LOG_INFO, "Starting watson-daemon");
+
+    config = load_iotp_config();
+    if (!config) {
+        rc = EXIT_FAILURE;
+        goto err1;
+    }
+
+    device = connect_device(config);
+    if (!device) {
+        rc = EXIT_FAILURE;
+        goto err2;
+    }
+    
     char *data = "{\"d\" : {\"SensorID\": \"Test\", \"Reading\": 7 }}";
     IOTPRC err = IoTPDevice_sendEvent(device, "status", data, "json", QoS1, NULL);
-    printf("%d\n", err);
-
-    IoTPDevice_disconnect(device);
-    IoTPDevice_destroy(device);
-    IoTPConfig_clear(config);
-
-    return 0;
+    
+    disconnect_device(device);
+err2:
+    clear_config(config);
+err1:
+    syslog(LOG_INFO, "Stopping watson-daemon");
+    closelog();
+    return rc;
 }
